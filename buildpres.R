00dd163e62e3da19e7d407db1283b3a49fc53987
Steph <Steph@itsalocke.com>
2017-06-15 15:42:15 +0100

    Revised content

--- buildpres.R
+++ buildpres.R
@@ -1,10 +1,10 @@
 library(rmarkdown)
-slides=list.files("pres","*.Rmd",full.names=TRUE)
+slides=list.files("slides","*.Rmd",full.names=TRUE)
 
 for (f in slides) render(f,output_dir = "docs")
 
 
 library(bookdown)
 setwd("handouts")
-render_book(output_format = bookdown::html_book(), output_dir = "../docs")
+render_book(input="index.Rmd", output_format = bookdown::html_book(), output_dir = "../docs")
 setwd("..")
